ed757b11bce00b0e186205a5295d7a0de526f59e
ahmed <ahmedelshehe49@gmail.com>
2020-05-11 15:57:08 +0200

    admin create page

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,1 +1,56 @@
-<?php
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    {!! Form::open(['action' => 'AdminUsersController@store','files'=>true]) !!}
+        <div class="form-group">
+        {{Form::label('name', 'User Name')}}
+        <br>
+        {{Form::text('name',null,['class'=>'form-control'])}}
+        </div>
+    <br>
+    <div class="form-group">
+        {{Form::label('email', 'E-Mail Address')}}
+        <br>
+        {{Form::text('email',null,['class'=>'form-control'])}}
+        </div>
+    <br>
+
+        <div class="form-group">
+        {{Form::label('password', 'Password')}}
+            <br>
+        {{Form::password('password', array('placeholder'=>'Password', 'class'=>'form-control' ) ) }}
+        </div>
+    <br>
+        <div class="form-group">
+        {{Form::label('file', 'Upload Photo')}}
+        <br>
+        {{Form::file('file',null,['class'=>'form-control'])}}
+        </div>
+    <br>
+        <div class="form-group">
+            {{Form::label('role_id','Role')}}
+            <br>
+            {{ Form::select('role_id', [''=>'Choose Options']+ $roles,2,['class'=>'form-control'])}}
+        </div>
+
+    <br>
+        <div class="form-group">
+        {{Form::label('is_active','Status')}}
+        <br>
+        {{ Form::select('is_active', [''=>'Choose Options',0=>'Not Active',1=>'Active'],null,['class'=>'form-control'])}}
+        </div>
+    {{Form::submit('Save',['class'=>'btn btn-primary'])}}
+
+    {!! Form::close() !!}
+@endsection
